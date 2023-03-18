86cbf970ee056b799044ddef575bc78cead8576a
ophx <castytastyhappybat@gmail.com>
2023-03-18 13:32:24 -0400

    Added get build number

--- commands/stats.js
+++ commands/stats.js
@@ -13,11 +13,24 @@ module.exports = {
       const seconds = Math.floor(uptime % 60);
       return `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
     }
+    function getCurrentBuildNumber() {
+      let comHash = require("child_process")
+        .execSync("git rev-parse --short HEAD", {cwd: __dirname})
+        .toString().trim();
+      let comHashLong = require("child_process")
+        .execSync("git rev-parse HEAD", {cwd: __dirname})
+        .toString().trim();
+
+      let comLink = (comHashLong) ? `https://github.com/PepeBOTrevolt/PepeBotSource/tree/${comHashLong}` : "https://github.com/PepeBOTrevolt/PepeBotSource";
+
+      return `${comHash}`;
+    }
 
     const embed = new SympactEmbedBuilder()
       .setDescription(`Servers: \`${message.client.servers.size}\`
       Users: \`${message.client.users.size}\`
-      Uptime: \`${getUptime()}\``);
+      Uptime: \`${getUptime()}\`
+      Build: \`${getCurrentBuildNumber()}\``);
     message.reply({ embeds: [embed] });
   },
 };

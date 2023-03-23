693a4ad71980d3e2709015ddccb04d3fc7540f37
ophx <69017612+ophx@users.noreply.github.com>
2023-03-23 14:26:00 -0400

    Create amogus.js

---
+++ commands/misc/amogus.js
@@ -0,0 +1,10 @@
+const { prefix } = require("../../config");
+
+module.exports = {
+  name: "amogus",
+  description: "sus...",
+  usage: `${prefix}amogus`,
+  async execute(message, args) {
+    message.reply(":01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6: :01GVY2FNM406Y178W46RTY7VK6:");
+  },
+};

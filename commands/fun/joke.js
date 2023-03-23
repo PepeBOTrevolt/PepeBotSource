365b89530d77dd88f1879e55ac58de9495bcc8c3
ophx <69017612+ophx@users.noreply.github.com>
2023-03-23 14:24:28 -0400

    Create joke.js

---
+++ commands/fun/joke.js
@@ -0,0 +1,23 @@
+const SympactEmbedBuilder = require("../../lib/embedBuilder");
+const { prefix } = require("../../config");
+
+module.exports = {
+  name: "joke",
+  description: "Generates a random joke.",
+  usage: `${prefix}joke`,
+  async execute(message, args) {
+    const response = await fetch("https://joke.deno.dev/");
+    const data = await response.json();
+
+    var emojis = [
+        "😂",
+        "😭",
+        "💀",
+    ];
+    var randomEmoji = Math.floor(Math.random()*emojis.length);
+
+    const embed = new SympactEmbedBuilder()
+      .setDescription(`${data.setup} ${data.punchline} ${emojis[randomEmoji]}`);
+    message.reply({ embeds: [embed] });
+  },
+};

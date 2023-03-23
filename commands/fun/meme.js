4d4f0f8f1dfe6b09526ed187909f71509711ee17
ophx <69017612+ophx@users.noreply.github.com>
2023-03-23 14:24:41 -0400

    Create meme.js

---
+++ commands/fun/meme.js
@@ -0,0 +1,13 @@
+const { prefix } = require("../../config");
+
+module.exports = {
+  name: "meme",
+  description: "Generates a random meme.",
+  usage: `${prefix}meme`,
+  async execute(message, args) {
+    const response = await fetch("https://meme-api.com/gimme");
+    const data = await response.json();
+
+    message.reply(`${data.title}\n${data.preview[2]}`);
+  },
+};
